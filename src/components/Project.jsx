079cab0af39a69b3d91aa394f5cd47248efdacde
uncenter <me@uncenter.org>
2023-02-14 08:47:01 -0500

    fix: open github page in new tab

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ import { mdiPause, mdiPlay, mdiHistory, mdiGithub } from "@mdi/js";
 export default function Project(){
     return(
         <div className="project">
-            <a href="https://github.com/uncenter/timer" target="_blank" rel="noopener noreferrer"><button className="github"><Icon path={mdiGithub} size={1} /></button></a>
+            <a href="https://github.com/uncenter/timer" target="_blank" rel="noopener noreferrer"><button className="github special"><Icon path={mdiGithub} size={1} /></button></a>
         </div>
     );
 }
